e5ca1704fd5c4024a491bc860717c799da334ec4
Michael Lange <dingoeatingfuzz@gmail.com>
2020-01-17 00:10:52 -0800

    Finish the heatmaps

--- src/HeatMap.svelte
+++ src/HeatMap.svelte
@@ -5,8 +5,10 @@
   import vegaEmbed from "vega-embed";
 
   let vegaDiv;
+  let vegaDiv2;
   let groups = [];
-  let chartData = [];
+  let chartDataSass = [];
+  let chartDataTotal = [];
 
   const countAll = gs => gs.reduce((sum, g) => sum + g.trees.length, 0);
   const median = list => {
@@ -19,57 +21,111 @@
   };
 
   const styleTimings = trees => trees.map(t => t.findStyleRoot().derivedTotal);
+  const totalTimings = trees => trees.map(t => t.root.derivedTotal);
+
   const buildMatch = to => from =>
     from.product === to.product && from.build === to.build;
 
-  const chart = data => {
+  const chart = (data, fieldFn) => {
     var dartGroups = data.filter(d => d.compiler === "dart");
     var libGroups = data.filter(d => d.compiler === "lib");
     return dartGroups.map(dartGroup => {
-      const dartMedian = median(styleTimings(dartGroup.trees));
+      const dartMedian = median(fieldFn(dartGroup.trees));
       const libMedian = median(
-        styleTimings(libGroups.find(buildMatch(dartGroup)).trees)
+        fieldFn(libGroups.find(buildMatch(dartGroup)).trees)
       );
+      const diff = 1 - dartMedian / libMedian;
       return {
         product: dartGroup.product,
         build: dartGroup.build,
         dartMedian,
         libMedian,
-        diff: 1 - dartMedian / libMedian
+        diff,
+        diffLabel: `${diff > 0 ? "dart" : "lib"}, +${(
+          Math.abs(diff) * 100
+        ).toFixed(2)}%|Δ${(Math.abs(dartMedian - libMedian) / 1000000).toFixed(
+          0
+        )}ms|${(Math.min(dartMedian, libMedian) / 1000000).toFixed(0)} v. ${(
+          Math.max(dartMedian, libMedian) / 1000000
+        ).toFixed(0)}ms`
       };
     });
   };
 
   const unsub = data.subscribe(d => {
     groups = d.data;
-    chartData = groups ? chart(groups) : [];
-    console.table(chartData);
+    chartDataSass = groups ? chart(groups, styleTimings) : [];
+    chartDataTotal = groups ? chart(groups, totalTimings) : [];
+  });
+
+  const vegaFor = (data, domain) => ({
+    $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+    description: "A simple bar chart with embedded data.",
+    data: {
+      values: data
+    },
+    width: 800,
+    height: 350,
+    encoding: {
+      x: {
+        field: "build",
+        type: "ordinal",
+        axis: { orient: "top", labelAngle: 0 }
+      },
+      y: { field: "product", type: "ordinal" }
+    },
+    layer: [
+      {
+        mark: "rect",
+        encoding: {
+          color: {
+            field: "diff",
+            type: "quantitative",
+            scale: { scheme: "blueorange", domain: [domain * -1, domain] }
+          }
+        }
+      },
+      {
+        mark: {
+          type: "text",
+          lineBreak: "|",
+          dy: -10
+        },
+        encoding: {
+          text: {
+            field: "diffLabel",
+            type: "nominal"
+          },
+          opacity: {
+            condition: { test: "datum['diff'] > 0", value: 0 },
+            value: 1
+          }
+        }
+      },
+      {
+        mark: {
+          type: "text",
+          lineBreak: "|",
+          dy: -10,
+          fontWeight: "bold"
+        },
+        encoding: {
+          text: {
+            field: "diffLabel",
+            type: "nominal"
+          },
+          opacity: {
+            condition: { test: "datum['diff'] > 0", value: 1 },
+            value: 0
+          }
+        }
+      }
+    ]
   });
 
   onMount(() => {
-    vegaDiv &&
-      vegaEmbed(vegaDiv, {
-        $schema: "https://vega.github.io/schema/vega-lite/v4.json",
-        description: "A simple bar chart with embedded data.",
-        data: {
-          values: [
-            { a: "A", b: 28 },
-            { a: "B", b: 55 },
-            { a: "C", b: 43 },
-            { a: "D", b: 91 },
-            { a: "E", b: 81 },
-            { a: "F", b: 53 },
-            { a: "G", b: 19 },
-            { a: "H", b: 87 },
-            { a: "I", b: 52 }
-          ]
-        },
-        mark: "bar",
-        encoding: {
-          x: { field: "a", type: "ordinal" },
-          y: { field: "b", type: "quantitative" }
-        }
-      });
+    vegaDiv && vegaEmbed(vegaDiv, vegaFor(chartDataSass, 7));
+    vegaDiv2 && vegaEmbed(vegaDiv2, vegaFor(chartDataTotal, 2));
   });
 </script>
 
@@ -78,8 +134,9 @@
     There are {groups.length} groups, totally {countAll(groups)} records..
   </h1>
   <div bind:this={vegaDiv} />
+  <div bind:this={vegaDiv2} />
   <!-- <FlameGraph timing={brokenRecords[0]} /> -->
-  <ol>
+  <!-- <ol>
     {#each groups as group}
       <li>
         <strong>
@@ -92,5 +149,5 @@
         </ol>
       </li>
     {/each}
-  </ol>
+  </ol> -->
 {/if}
